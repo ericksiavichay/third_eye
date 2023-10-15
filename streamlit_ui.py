f6870a3904050385d2d8b62f3b21aeab27843362
Baladhurgesh <baladhurgesh97@gmail.com>
2023-10-14 19:44:15 -0700

    Commented YOLO integration

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -27,7 +27,7 @@ def main():
 
         try:
             image_response = requests.post(
-                "http://p3frgvi5oe.loclx.io/uploadfile", files=image_files
+                "http://rrmllhdwgk.loclx.io/uploadfile", files=image_files
             )
             image_response.raise_for_status()
             response_json = image_response.json()
--- utils.py
+++ utils.py
@@ -6,61 +6,61 @@ from PIL import Image
 from transformers import Pix2StructForConditionalGeneration, Pix2StructProcessor
 from fastapi.responses import JSONResponse
 from io import BytesIO
-import cv2
-from ultralytics import YOLO
+# import cv2
+# from ultralytics import YOLO
 
 app = FastAPI()
 model = Pix2StructForConditionalGeneration.from_pretrained(
     "google/pix2struct-textcaps-base"
 ).to("cuda")
 processor = Pix2StructProcessor.from_pretrained("google/pix2struct-textcaps-base")
-image_to_text = YOLO("yolov8n.pt")
+# image_to_text = YOLO("yolov8n.pt")
 
 
-def yolo_single_frame(frame):
-    some_return = model.predict(frame, show=True, stream=True)
-    return some_return
+# def yolo_single_frame(frame):
+#     some_return = model.predict(frame, show=True, stream=True)
+#     return some_return
 
 
-def stream_object_detection():
-    cap = cv2.VideoCapture(0)
+# def stream_object_detection():
+#     cap = cv2.VideoCapture(0)
 
-    if not cap.isOpened():
-        print("Error: Could not open camera.")
-        exit()
+#     if not cap.isOpened():
+#         print("Error: Could not open camera.")
+#         exit()
 
-    while True:
-        # Read a frame from the webcam
-        ret, frame = cap.read()
-        if not ret:
-            print("Error: Couldn't read frame.")
-            break
+#     while True:
+#         # Read a frame from the webcam
+#         ret, frame = cap.read()
+#         if not ret:
+#             print("Error: Couldn't read frame.")
+#             break
 
-        # Use YOLO to predict on the frame directly
-        some_return = model.predict(frame, show=True, stream=True)
-        #
+#         # Use YOLO to predict on the frame directly
+#         some_return = model.predict(frame, show=True, stream=True)
+#         #
 
-        # Display the frame
-        cv2.imshow("YOLO Real-time Detection", frame)
+#         # Display the frame
+#         cv2.imshow("YOLO Real-time Detection", frame)
 
-        # Wait for keypress
-        key = cv2.waitKey(2)  # you'll have to play around with the amount of time
+#         # Wait for keypress
+#         key = cv2.waitKey(2)  # you'll have to play around with the amount of time
 
-        # Check if the pressed key is the spacebar
-        if key == 32:
-            # Capture user prompt
-            user_prompt = input("Enter your prompt: ")
+#         # Check if the pressed key is the spacebar
+#         if key == 32:
+#             # Capture user prompt
+#             user_prompt = input("Enter your prompt: ")
 
-            inputs = processor(frame, return_tensors="pt", padding="max_length")
-            out = image_to_text.generate(**inputs)
-            generated_text = processor.decode(out[0], skip_special_tokens=True)
+#             inputs = processor(frame, return_tensors="pt", padding="max_length")
+#             out = image_to_text.generate(**inputs)
+#             generated_text = processor.decode(out[0], skip_special_tokens=True)
 
-            print("-" * 50)
-            print(f"Generated Text: {generated_text}")
-            print("-" * 50)
-            cap.release()
-            cv2.destroyAllWindows()
-            return generated_text, frame
+#             print("-" * 50)
+#             print(f"Generated Text: {generated_text}")
+#             print("-" * 50)
+#             cap.release()
+#             cv2.destroyAllWindows()
+#             return generated_text, frame
 
 
 def model_inference(input_image, input_prompt=""):
@@ -69,10 +69,10 @@ def model_inference(input_image, input_prompt=""):
     inputs = processor(images=image, return_tensors="pt").to("cuda")
     start_time = time.time()
     predictions = model.generate(**inputs)
-    yolo_returns = yolo_single_frame(image)
+    # yolo_returns = yolo_single_frame(image)
     end_time = time.time()
 
     response = processor.decode(predictions[0], skip_special_tokens=True)
-    print(response, yolo_returns)
+    print(response)
     print(end_time - start_time)
     return response
